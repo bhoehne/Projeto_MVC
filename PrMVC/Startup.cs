5bc16f598cb27a3b1935eae149a40e1f3f805175
bhoehne <bhoehne@tutanota.com>
2021-11-18 11:38:21 -0300

    created seed, rebase database

--- PrMVC/Startup.cs
+++ PrMVC/Startup.cs
@@ -38,14 +38,17 @@ namespace PrMVC
 
             services.AddDbContext<PrMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("PrMVCContext"), builder => builder.MigrationsAssembly("PrMVC")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
